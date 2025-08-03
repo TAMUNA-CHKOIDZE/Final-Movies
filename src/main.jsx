e75ea6ba18aa4b2dabda62d26d31a1497f4ff39a
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-08-03 11:59:26 +0400

    Switch BrowserRouter to HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 import { MovieProvider } from "./context/MovieContext";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <MovieProvider>
         <App />
       </MovieProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
-
-
